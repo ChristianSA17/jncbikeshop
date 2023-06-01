fce02c2d3984c4db0328eadfc742a918f27a8166
ChristianSA <cjsanagustin172003@gmail.com>
2023-06-01 14:24:04 +0800

    Changing the logo, adding menu button, removing, search and add to cart button, adding preloader, adding dropdown button on the product button, also adding the notes for the add to cart and search icon html and css

--- javascript.js
+++ javascript.js
@@ -4,3 +4,11 @@ checkbox.addEventListener('change', () => {
     // change the theme of the website
     document.body.classList.toggle('dark');
 });
+
+var preloader = document.querySelector(".preloader") 
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+    preloader.classList.add("disappear")
+}
