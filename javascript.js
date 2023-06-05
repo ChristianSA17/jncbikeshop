723236bce77b4849618a48781c1d080fdbe41095
ChristianSA <cjsanagustin172003@gmail.com>
2023-06-05 23:47:01 +0800

    adding automatic carousel slider

--- javascript.js
+++ javascript.js
@@ -14,28 +14,53 @@ function vanish() {
 }
 
 let slideIndex = 1;
+let slideTimer;
+
 showSlides(slideIndex);
+startSlideTimer();
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
+    resetSlideTimer();
 }
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
+    resetSlideTimer();
 }
 
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    
+    if (n > slides.length) {
+        slideIndex = 1;
     }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
+    }
+    
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
+
+function startSlideTimer() {
+    slideTimer = setInterval(function() {
+        showSlides(slideIndex += 1);
+    }, 5000);
+}
+
+function resetSlideTimer() {
+    clearInterval(slideTimer);
+    startSlideTimer();
+}
+
